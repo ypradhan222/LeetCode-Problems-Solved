df59faa594463326ab6671900952bcbba200b3ef
Yogen Pradhan <98086826+ypradhan222@users.noreply.github.com>
2024-07-29 07:29:28 +0200

    Time: 19 ms (27.34%), Space: 56.6 MB (71.36%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,21 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         for(int num : nums){
+//             map.put(num,map.getOrDefault(num,0)+1); 
+//         }
+//         return map.containsValue(2);
+//         }
+//     }
+  class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int num : nums) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+                if (map.get(num) > 1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
