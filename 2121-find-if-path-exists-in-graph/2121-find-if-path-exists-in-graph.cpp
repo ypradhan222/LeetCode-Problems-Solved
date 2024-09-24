92cfa63fc583eafefada4d268e6f73d6f9dd140b
Yogen Pradhan <98086826+ypradhan222@users.noreply.github.com>
2024-09-24 14:40:31 +0530

    Time: 1143 ms (29.4%), Space: 358.7 MB (24.26%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -7,19 +7,38 @@ public:
             mp[vec[1]].push_back(vec[0]);
         }
         vector<bool> visited(n,false);
-        return dfs(mp,source,destination,visited,n);
-    }
-    bool dfs(unordered_map<int,vector<int>>& graph,int source,int destination,vector<bool>& visited,int n){
-        if(source==destination){
-            return true;
-        }
         visited[source]= true;
-        for(auto neighbor: graph[source]){
-            if(!visited[neighbor]){
-                if(dfs(graph,neighbor,destination,visited,n)) return true;;
+        return bfs(mp,source,destination,visited,n);
+    }
+    // bool dfs(unordered_map<int,vector<int>>& graph,int source,int destination,vector<bool>& visited,int n){
+    //     if(source==destination){
+    //         return true;
+    //     }
+    //     visited[source]= true;
+    //     for(auto neighbor: graph[source]){
+    //         if(!visited[neighbor]){
+    //             if(dfs(graph,neighbor,destination,visited,n)) return true;;
+    //         }
+    //     }
+
+    //     return false;
+    // }
+    bool bfs(unordered_map<int,vector<int>>& graph,int source,int destination,vector<bool>& visited,int n){
+        queue<int> q;
+        q.push(source);
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            if(curr==destination){
+                return true;
+            }
+            for(int neighbor:graph[curr]){
+                if(!visited[neighbor]){
+                    q.push(neighbor);
+                    visited[neighbor]= true;
+                }
             }
         }
-
         return false;
     }
 };
