673c1b0c72fd547c56ad86b57fea317bc3258f63
Yogen Pradhan <98086826+ypradhan222@users.noreply.github.com>
2024-09-26 10:57:59 +0530

    Time: 15 ms (59.96%), Space: 30 MB (46.96%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     int numberOfPoints(vector<vector<int>>& nums) {
-       set<int> st;
-       for(vector<int> num:nums){
+    //Using Set
+    //    set<int> st;
+    //    for(vector<int> num:nums){
+    //     int start = num[0];
+    //     int end = num[1];
+    //     for(int i=start;i<=end;i++){
+    //         st.insert(i);
+    //     }
+    //    }
+    // //    int count =0;
+    //    for (auto it = st.begin(); it != 
+    //                       st.end(); ++it)
+    //     cout << ' ' << *it;
+
+    //    return st.size();
+    
+    //Using counting
+    vector<int> freq(102,0);
+    for(vector<int> num:nums){
         int start = num[0];
         int end = num[1];
-        for(int i=start;i<=end;i++){
-            st.insert(i);
-        }
-       }
-    //    int count =0;
-       for (auto it = st.begin(); it != 
-                          st.end(); ++it)
-        cout << ' ' << *it;
-
-       return st.size();
+        freq[start]++;
+        freq[end+1]--;
+    }
+    int count=0;
+    for(int i=1;i<freq.size();i++){
+        freq[i]+=freq[i-1];
+        if(freq[i]>0) count++;
+    }
+    return count;
     }
 };
