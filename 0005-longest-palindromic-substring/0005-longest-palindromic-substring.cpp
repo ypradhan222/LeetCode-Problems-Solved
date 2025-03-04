8d1a584af1e1218d5059144618046192751a7628
Yogen Pradhan <98086826+ypradhan222@users.noreply.github.com>
2025-03-04 10:30:21 +0530

    Time: 213 ms (23.79%), Space: 291.8 MB (11.89%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string answer="";
-        int maxlength=0;
-       vector<vector<int>> dp(s.length(),vector<int>(s.length(),0));
-       for(int diff=0;diff<s.length();diff++){
-        for(int i=0,j=i+diff;j<s.length();i++,j++){
-            if(i==j){
-                dp[i][j] =1;
-            }
-            else if (diff==1){
-                dp[i][j] =(s[i]==s[j])?2:0;
-            }
-            else{
-                if(s[i]==s[j] && dp[i+1][j-1]){
-                    dp[i][j] = dp[i+1][j-1]+2;
+        int n=s.size();
+        int maxlen=0;
+        vector<vector<int>> t(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            t[i][i]=1;
+            maxlen=1;
+        }
+        int idx=0;
+        for(int len=2;len<=n;len++){
+            for(int i=0;i+len-1<n;i++){
+                int j=i+len-1;
+                if(s[i]==s[j] && len==2){
+                    t[i][j]=1;
+                    maxlen=len;
+                    idx=i;
+                }
+                else if(s[i]==s[j] && t[i+1][j-1]){
+                    t[i][j]=1;
+                    maxlen=max(maxlen,j-i+1);
+                    idx=i;
                 }
             }
-            if(dp[i][j]){
-                maxlength = max(j-i+1,maxlength);
-                answer = s.substr(i,maxlength);
-            }
         }
-       } 
-       return answer;
+        return s.substr(idx,maxlen);
     }
-    
 };
