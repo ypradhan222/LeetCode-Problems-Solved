56ac8a163ce38588f1e419f5b22460a8d248227a
Yogen Pradhan <98086826+ypradhan222@users.noreply.github.com>
2025-04-15 21:30:08 +0530

    Time: 187 ms (5.02%), Space: 56.1 MB (5.07%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,26 +3,27 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int m= grid.size();
         int n = grid[0].size();
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        // vector<vector<bool>> visited(m,vector<bool>(n,false));
         int count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='1' && !visited[i][j]){
+                if(grid[i][j]=='1'){
                     count++;
-                    dfs(grid,visited,i,j,m,n);
+                    dfs(grid,i,j,m,n);
                 }
             }
         }
         return count;
     }
-    void dfs(vector<vector<char>>& grid,vector<vector<bool>>& visited,int i,int j,int m,int n){
-        visited[i][j]= true;
+    void dfs(vector<vector<char>>& grid,int i,int j,int m,int n){
+        if(grid[i][j]==-1) return;
+        grid[i][j]=-1;
         vector<vector<int>>  directions= {{0,-1},{0,1},{-1,0},{1,0}};
         for(int k=0;k<4;k++){
             int newi = i+directions[k][0];
             int newj =j+directions[k][1];
-            if(safePosition(newi,newj,m,n) && grid[newi][newj]=='1' && !visited[newi][newj]){
-                dfs(grid,visited,newi,newj,m,n);
+            if(safePosition(newi,newj,m,n) && grid[newi][newj]=='1'){
+                dfs(grid,newi,newj,m,n);
             }
         }
     }
